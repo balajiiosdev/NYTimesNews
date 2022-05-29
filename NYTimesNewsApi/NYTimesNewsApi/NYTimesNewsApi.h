1fc935a704453ed2ab4275ff59d2cb9b175f9be6
Balaji V <balaji.iosdev@gmail.com>
2022-05-29 16:00:18 +0530

    NYTimesNewsApi framework project has been created
    - Purpose of this framework is to have all apis implemented in this framework
    -This speeds up compile time when the number of API classes increased .
    - Allows separation of Network layer from application logic

---
+++ NYTimesNewsApi/NYTimesNewsApi/NYTimesNewsApi.h
@@ -0,0 +1,18 @@
+//
+//  NYTimesNewsApi.h
+//  NYTimesNewsApi
+//
+//  Created by Balaji V on 5/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NYTimesNewsApi.
+FOUNDATION_EXPORT double NYTimesNewsApiVersionNumber;
+
+//! Project version string for NYTimesNewsApi.
+FOUNDATION_EXPORT const unsigned char NYTimesNewsApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYTimesNewsApi/PublicHeader.h>
+
+
