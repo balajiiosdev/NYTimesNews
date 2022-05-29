97f17029b577580ba2cef754c1cd9704480013df
Balaji V <balaji.iosdev@gmail.com>
2022-05-29 21:13:56 +0530

    HTTPMethod enum is added
    Unit tests are written to validate the raw values to ensure they are unchanged

---
+++ NYTimesNewsApi/NYTimesNewsApi/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  NYTimesNewsApi
+//
+//  Created by Balaji V on 5/29/22.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
---
+++ NYTimesNewsApi/NYTimesNewsApiTests/HTTPMethodTests.swift
@@ -0,0 +1,20 @@
+//
+//  HTTPMethodTests.swift
+//  NYTimesNewsApiTests
+//
+//  Created by Balaji V on 5/29/22.
+//
+
+import XCTest
+@testable import NYTimesNewsApi
+
+class HTTPMethodTests: XCTestCase {
+    func testHttpMethodRawValues() {
+        XCTAssertEqual(HTTPMethod.get.rawValue, "GET")
+        XCTAssertEqual(HTTPMethod.post.rawValue, "POST")
+        XCTAssertEqual(HTTPMethod.put.rawValue, "PUT")
+        XCTAssertEqual(HTTPMethod.delete.rawValue, "DELETE")
+        XCTAssertEqual(HTTPMethod.patch.rawValue, "PATCH")
+    }
+
+}
