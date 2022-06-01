1dfecf929cde95379af4c308b04e03861c0f588b
Balaji V <balaji.iosdev@gmail.com>
2022-06-01 13:02:36 +0530

    Basic Table View is added to show the title of article

--- NYTimesNews/Scenes/NewsList/NewsListViewController.swift
+++ NYTimesNews/Scenes/NewsList/NewsListViewController.swift
@@ -17,6 +17,8 @@ protocol NewsListDisplayLogic: AnyObject {
 class NewsListViewController: UIViewController {
     var interactor: NewsListBusinessLogic?
     var router: (NSObjectProtocol & NewsListRoutingLogic & NewsListDataPassing)?
+    var viewModel: NewsList.TopNews.ViewModel?
+    var tableView: UITableView!
 
     // MARK: Initialisers
     required init?(coder aDecoder: NSCoder) {
@@ -45,7 +47,26 @@ class NewsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = NSLocalizedString("top_news_title", comment: "")
+        addTableView()
         fetchTopNews()
+    }
+
+    private func addTableView() {
+        tableView = UITableView(frame: CGRect.zero, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        addConstraintsToTableView()
+    }
+
+    private func addConstraintsToTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 
     // MARK: Top News
@@ -58,11 +79,37 @@ class NewsListViewController: UIViewController {
 
 extension NewsListViewController: NewsListDisplayLogic {
     func displayTopNews(viewModel: NewsList.TopNews.ViewModel) {
+        self.viewModel = viewModel
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
 
     func displayErrorAlert(title: String?, message: String?) {
     }
 
     func displayToast(message: String) {
+    }
+}
+
+extension NewsListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.articles.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel?.articles[indexPath.row].title
+        cell.detailTextLabel?.text = viewModel?.articles[indexPath.row].author
+        return cell
+    }
+}
+
+extension NewsListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 }
